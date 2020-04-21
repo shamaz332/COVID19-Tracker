d831ea74dc123335dc2d987a3c79e3041ead2c55
shamaz saeed <shamazsaeed332.com>
2020-04-21 21:54:47 +0500

    Style Changed

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 import axios from "axios";
 import "../App.css";
 class Home extends Component {
@@ -32,22 +32,24 @@ class Home extends Component {
   render() {
     const allCountry = this.state.countries.map((country, i) => {
       return (
-<div lat = {country.countryInfo.lat}
-lng = {country.countryInfo.long}
-style={{
-  color :"red",
-  backgroundColor : "#FFF",
-  height : "50px",
-  width : "50px",
-  textAlign:"center",
-  fontSize:"14px",
-  fontWeight:"bold",
-  borderRadius:"30%"
-}}>
-  <img height="10px" src={country.countryInfo.flag}/>
-<br></br>
-{country.cases}
-</div>
+        <div
+          lat={country.countryInfo.lat}
+          lng={country.countryInfo.long}
+          style={{
+            color: "red",
+            backgroundColor: "#FFF",
+            height: "50px",
+            width: "50px",
+            textAlign: "center",
+            fontSize: "14px",
+            fontWeight: "bold",
+            borderRadius: "30%",
+          }}
+        >
+          <img height="10px" src={country.countryInfo.flag} />
+          <br></br>
+          {country.cases}
+        </div>
       );
     });
 
@@ -56,8 +58,8 @@ style={{
         <Container>
           <Row>
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="cc shadow p-3 bg-white rounded">
-                <Card.Header className="header text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Total Case</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -68,8 +70,8 @@ style={{
               </Card>
             </Col>
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Total Deaths</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -80,8 +82,8 @@ style={{
               </Card>
             </Col>
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Recovered</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -97,8 +99,8 @@ style={{
 
           <Row>
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Today Case</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -109,8 +111,8 @@ style={{
               </Card>
             </Col>
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Today Deaths</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -121,8 +123,8 @@ style={{
               </Card>
             </Col>
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Active Case</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -139,8 +141,8 @@ style={{
 
           <Row>
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Critical</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -152,8 +154,8 @@ style={{
             </Col>
 
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Tested Patients</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -165,8 +167,8 @@ style={{
             </Col>
 
             <Col sm={12} md={4} lg={4} xl={4} className="mt-4 mb-2">
-              <Card className="shadow p-3 bg-white rounded">
-                <Card.Header className="text-center">
+              <Card className="title-box shadow p-3 bg-white rounded">
+                <Card.Header className="header-box text-center">
                   <h5>Countries</h5>
                 </Card.Header>
                 <Card.Body className="text-center p-4">
@@ -179,17 +181,18 @@ style={{
           </Row>
         </Container>
 
-<br></br>
-        <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key:"AIzaSyB_ZZuFi35J3-17e64iqegfQZPphNn2vto"}}
-          defaultCenter={{  lat: 30.3753,
-            lng:69.3451}}
-          defaultZoom={4}
-        >
-{allCountry}
-        </GoogleMapReact>
-      </div>
+        <br></br>
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyB_ZZuFi35J3-17e64iqegfQZPphNn2vto",
+            }}
+            defaultCenter={{ lat: 30.3753, lng: 69.3451 }}
+            defaultZoom={4}
+          >
+            {allCountry}
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
